a44d84c7315a5de8ea1fd24d0180ccf360de4c67
Jessica Brown <jessica@jbrowns.com>
2022-04-21 14:32:50 -0400

    Added Git Ignores to directory structure

--- install.sh
+++ install.sh
@@ -15,16 +15,20 @@ txtReset='\e[38;5;8m'
 Cyan='\e[38;5;51m'
 txtReset='\e[0m'
 
-[ -f "${scriptLocation}/etc/conf/collector.shlib" ] && source "${scriptLocation}/etc/conf/collector.shlib" || echo "${scriptLocation}/etc/conf/collector.shlib"
-[ -f "${scriptLocation}/etc/setdirectories" ] && source "${scriptLocation}/etc/setdirectories" || echo "${scriptLocation}/etc/setdirectories"
-[ -f "${eb3_BinPath}logprocess" ] && source "${eb3_BinPath}logprocess" || echo "${eb3_BinPath}logprocess"
+[ -f "${scriptLocation}/etc/conf/collector.shlib" ] && source "${scriptLocation}/etc/conf/collector.shlib" || echo "Error loading ${scriptLocation}/etc/conf/collector.shlib"
+[ -f "${scriptLocation}/etc/setdirectories" ] && source "${scriptLocation}/etc/setdirectories" || echo "Error loading ${scriptLocation}/etc/setdirectories"
+[ -f "${eb3_BinPath}logprocess" ] && source "${eb3_BinPath}logprocess" || echo "Error loading ${eb3_BinPath}logprocess"
 
 for folder in "${eb3_systemFolders[@]}"; do
 	if [[ -d ${folder} ]]; then
-		for file in "${folder}"/???_*; do
+		for file in "${folder}"???_*; do
 			if [[ -f ${file} ]]; then
 				source "${file}"
-				success "[Loading ${file}]" >> "${eb3_LogsPath}install.log"
+				if [ $? -eq 0 ]; then
+                    success "Loading ${file}" >> "${eb3_LogsPath}install.log"
+                else
+                    error "Loading ${file}" >> "${eb3_LogsPath}install.log"
+                fi
 			fi
 		done
 	fi
